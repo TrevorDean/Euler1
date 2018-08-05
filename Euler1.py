ee3f0d4e82f45701e3be1872b8733077de585b15
Trevor <TrevorDean@users.noreply.github.com>
2018-08-04 23:04:24 -0500

    Working Euler1 Solution

---
+++ Euler1.py
@@ -0,0 +1,16 @@
+def multiplefinder35(number):
+    counter = 0
+    numberstore = []
+    while number > 0:
+        if number % 3 == 0:
+            counter += 1
+            numberstore.append(number)
+        elif number % 5 == 0:
+            counter += 1
+            numberstore.append(number)
+        number -= 1
+
+    return sum(numberstore)
+
+print multiplefinder35(999)
+
